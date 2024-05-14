9b5628e5e41367650b26410590457217126f5775
hnin <hnin0929@ecplaza.net>
2024-05-14 17:58:19 +0900

    fetching cabin data

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,25 +1,41 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 
-const Table = styled.div`
-  border: 1px solid var(--color-grey-200);
+import { useQuery } from "@tanstack/react-query"
+import { getCabins } from "../../services/apiCabins"
 
-  font-size: 1.4rem;
-  background-color: var(--color-grey-0);
-  border-radius: 7px;
-  overflow: hidden;
-`;
+// const Table = styled.div`
+//   border: 1px solid var(--color-grey-200);
 
-const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
+//   font-size: 1.4rem;
+//   background-color: var(--color-grey-0);
+//   border-radius: 7px;
+//   overflow: hidden;
+// `;
 
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
-`;
+// const TableHeader = styled.header`
+//   display: grid;
+//   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+//   column-gap: 2.4rem;
+//   align-items: center;
+
+//   background-color: var(--color-grey-50);
+//   border-bottom: 1px solid var(--color-grey-100);
+//   text-transform: uppercase;
+//   letter-spacing: 0.4px;
+//   font-weight: 600;
+//   color: var(--color-grey-600);
+//   padding: 1.6rem 2.4rem;
+// `;
+
+export default function CabinTable() {
+
+  const x = useQuery({
+    queryKey: ['cabin'],
+    queryFn: getCabins
+  });
+  console.log(x);
+
+  return (
+    <div>CabinTable</div>
+  );
+}
